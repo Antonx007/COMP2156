82cef61275b32893542ae20205497ba340f3be13
Antonx007 <anatolisen@gmail.com>
2025-02-09 22:22:17 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101529881</h1>
+      <h1>Student Name: Anatoli Sengiyumva</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Anatoli Sengiyumva
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
